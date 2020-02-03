5b0fdad09aaa1f8655c5bad657bad378ff8e975c
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2020-02-03 08:28:49 -0500

    Started work on file input

---
+++ Final Project/brailleProcessor.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string inputFileName = " ";
+    
+    cout << "Enter name of input file - must be in same directory as script (ex. Input.txt)\n";
+    cin >> inputFileName;
+
+    ifstream fin(inputFileName);
+    // TODO - Add ofstream directly into robot
+
+    // Process input
+        // Process word
+        // Check if line in space
+            //Process char
+        // Space
+
+}
