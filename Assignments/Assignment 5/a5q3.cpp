c2319669f6f494ab78d308619da9203ea22786fe
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2020-02-05 14:30:50 -0500

    Added Lab Partner Names to Assignment 5

--- Assignments/Assignment 5/a5q3.cpp
+++ Assignments/Assignment 5/a5q3.cpp
@@ -1,4 +1,4 @@
-// Chamath Wijesekera - ME 101
+// Chamath Wijesekera and Gauthamkrishna Anil - ME 101
 // Assignment 5 Question 3
 // Original Cell Stucture Program Code by Chamath Wijesekera and Tarj Tandel
 
@@ -59,6 +59,26 @@ int main()
     return EXIT_SUCCESS;   
 }
 
+/*
+Enter the type of cell structure (SC, BCC, or FCC): ABC
+Enter the type of cell structure (SC, BCC, or FCC): 
+
+Enter the type of cell structure (SC, BCC, or FCC): SC
+Enter radius in Angstroms: -1
+Enter radius in Angstroms: 
+
+Enter the type of cell structure (SC, BCC, or FCC): SC
+Enter radius in Angstroms: 2
+The volume of this SC cell is 64
+
+Enter the type of cell structure (SC, BCC, or FCC): BCC
+Enter radius in Angstroms: 3
+The volume of this BCC cell is 332.554
+
+Enter the type of cell structure (SC, BCC, or FCC): FCC
+Enter radius in Angstroms: 5.5
+The volume of this FCC cell is 3764.64
+*/
 
 
 
