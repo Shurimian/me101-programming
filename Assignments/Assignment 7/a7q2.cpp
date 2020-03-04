126997e552fd684e2229745ab6b321df657522ba
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2020-03-04 15:55:00 -0500

    Update a7q2.cpp

--- Assignments/Assignment 7/a7q2.cpp
+++ Assignments/Assignment 7/a7q2.cpp
@@ -9,39 +9,35 @@
 
 using namespace std;
 
-
 double calcYoungsMod(int measurementCount, double strains[], double stresses[])
 {
-    int fivePercentOfSamples = round(measurementCount * 0.05);
-    double sumStress = 0;
-    double sumStrain = 0;
-    
+    int fivePercentOfSamples = measurementCount * 0.05;
+    double sumSlope = 0;
+
     for (int count = 0; count < fivePercentOfSamples; count++)
     {
-        sumStrain += strains[count];
-        sumStress += stresses[count];
+        sumSlope += (stresses[count + 1] - stresses[count]) / (strains[count + 1] - strains[count]);
     }
 
-    return (sumStress / fivePercentOfSamples) / (sumStrain / fivePercentOfSamples);
+    return sumSlope / fivePercentOfSamples;
 }
 
-double calcYieldStrength(double youngsMod, double strains[], double stresses[], int pointTwoIndex)
+double calcYieldStrength(double youngsMod, double strains[], double stresses[])
 {
     const double YIELD_STRENGTH_TOLERANCE = 1;
     
     double interceptDiff = 0;
     double curveStress = 0;
     double modulusStress = 0;
-    int index = pointTwoIndex;
-    
+    double yInt = -(youngsMod * 0.002);
+    int index = 0;
+
     do
     {
         curveStress = stresses[index];
-        modulusStress = youngsMod * (strains[index] - 0.002);
-        
+        modulusStress = youngsMod * strains[index] + yInt;
         interceptDiff = abs(curveStress - modulusStress);
         index++;
-
     } while (interceptDiff > YIELD_STRENGTH_TOLERANCE);
 
     return stresses[index];
@@ -67,7 +63,6 @@ int main()
     double strain[MAX_FILE_SAMPLES] = {0};
     double stressMPa[MAX_FILE_SAMPLES] = {0};
     double ultimateTensileStr = -1;
-    double pointTwoStrainIndex = 0;
 
     string ignore = "";
     fin >> ignore >> ignore;
@@ -81,25 +76,34 @@ int main()
             ultimateTensileStr = stressMPa[index];
         }
 
-        const double POINT_TWO_TOLERANCE = 0.0001;
-
-        if (abs(strain[index] - 0.002) < POINT_TWO_TOLERANCE)
-        {
-            pointTwoStrainIndex = index;
-        }
-        
         index++;
     }
 
     double youngsMod = calcYoungsMod(index, strain, stressMPa);
-    double yieldStrength = calcYieldStrength(youngsMod, strain, stressMPa, pointTwoStrainIndex);
+    double yieldStrength = calcYieldStrength(youngsMod, strain, stressMPa);
 
-    cout << fixed << setprecision(3)
+    cout << fixed << setprecision(2)
          << "The Ultimate Tensile Strength is: " << ultimateTensileStr << " MPa\n"
          << "The Young's Modulus is: " << youngsMod << " MPa\n"
-         << "The Yield Strength is: " << yieldStrength << "MPa\n"; 
+         << "The Yield Strength is: " << yieldStrength << " MPa\n"; 
 
     fin.close();
     return EXIT_SUCCESS;
 }
 
+/* 
+Enter name of input file (ex. Sample1.txt): Sample1.txt
+The Ultimate Tensile Strength is: 386.832 MPa
+The Young's Modulus is: 8813.724 MPa
+The Yield Strength is: 324.328 MPa
+
+Enter name of input file (ex. Sample1.txt): Sample2.txt
+The Ultimate Tensile Strength is: 309.157 MPa
+The Young's Modulus is: 9121.793 MPa
+The Yield Strength is: 301.333MPa
+
+Enter name of input file (ex. Sample1.txt): Sample3.txt
+The Ultimate Tensile Strength is: 386.832 MPa
+The Young's Modulus is: 4901.723 MPa
+The Yield Strength is: 357.846MPa
+*/
