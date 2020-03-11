0269906ee39c3a3efe14557fc97f7829923b5712
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2020-03-10 21:47:51 -0400

    Began work on array implementation for assignment

--- Assignments/Assignment 8/a8q1.cpp
+++ Assignments/Assignment 8/a8q1.cpp
@@ -1,3 +1,28 @@
 // Chamath Wijesekera and Emily Tou - ME 101
 // Assignment 8 Question 1
 
+/* 
+ARRAY IMPLEMENTATIONN
+
+Composed of a 2D array consisting of a row for status and a row for name. Columns will be 50 elements long to represent 50 parking spaces. 
+
+Parking Current
+    Composed of a 2D array consisting of three rows. Each row stores data as follows:
+        Row 1 - Occupation
+          1 = staff
+          0 = student
+          -1 = empty 
+        Row 2 - Name
+
+Parking Add / Parking Remove
+    Composed of a 2D array consisting of three rows. Each row stores data as follows:
+        Row 1 - Occupation
+          1 = staff
+          0 = student
+        Row 2 - Name
+        Row 3 - Change
+          1 = add to parking
+          -1 = remove from parking
+
+Empty parking spots will be represented as "-1" in the occupation row in the array
+*/
